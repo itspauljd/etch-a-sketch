284b8feeccc1d8c6f9b5e4d6dd9996e5e1f274a7
Paul Park <paulparkjd@gmail.com>
2022-08-30 20:15:22 -0700

    Deleted script and changed styles

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const container = document.getElementById("container");
 
-for (i = 0; i < 16; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add('col')
-    container.appendChild(cell);
-}
+
