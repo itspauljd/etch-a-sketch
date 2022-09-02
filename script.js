63fbdd7f0176f2ffd58b0e9a93d60ab2e16ee9d5
Paul Park <paulparkjd@gmail.com>
2022-09-01 17:53:26 -0700

    Added functionality to 'reset' button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const grid = document.getElementById('grid');
+const resetButton = document.getElementById('reset');
 
+
+// This function makes the grid and adds on hover events
 function makeGrid(size) {
     let cellLength = 550 / size;
     for (i = 0; i < size ** 2; i++) {
@@ -7,16 +10,28 @@ function makeGrid(size) {
         cell.classList.add('gridCell');
         cell.style.width = cellLength + 'px'
         cell.style.length = cellLength + 'px'
+        cell.addEventListener('mouseover', makeColor);
+        function makeColor() {
+            cell.style.backgroundColor = 'black';
+        }
         if (i == 0) {
-            cell.style.borderTopLeftRadius = 10 + 'px' 
+            cell.style.borderTopLeftRadius = 10 + 'px'
         } else if (i == size - 1) {
             cell.style.borderTopRightRadius = 10 + 'px'
         } else if (i == size ** 2 - size) {
             cell.style.borderBottomLeftRadius = 10 + 'px'
-        } else if (i == size ** 2 -1) {
+        } else if (i == size ** 2 - 1) {
             cell.style.borderBottomRightRadius = 10 + 'px'
         }
         grid.appendChild(cell);
+    }
+}
+
+resetButton.addEventListener('click', reset);
+function reset() {
+    const collection = document.getElementsByClassName("gridCell");
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.backgroundColor = 'rgb(194, 179, 179)';
     }
 }
 
